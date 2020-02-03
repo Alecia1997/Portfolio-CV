7bc42fe357195a9915861d55feb2cb2283213d13
Alecia1997 <vermeulenalecia344@gmail.com>
2020-02-03 22:50:49 +0200

    First commit

---
+++ assets/js/functions.js
@@ -0,0 +1,49 @@
+function openBrief1() {
+  window.open('Reversi.pdf');
+}
+
+function openCode1() {
+    window.open("https://github.com/Alecia1997/ReversiGame-Java-JavaFX.git");
+}
+
+function openBrief2() {
+  window.open('lilCup.pdf');
+}
+
+function openCode2() {
+    window.open("https://github.com/Alecia1997/CoffeeWebsite-Javascript.git");
+}
+ 
+function openBrief3() {
+  window.open('honours.pdf');
+}
+
+function openCode3() {
+    window.open("https://github.com/Alecia1997/TobiasPark.git");
+}
+ 
+function openBrief4() {
+  window.open('flights.pdf');
+}
+
+function openCode4() {
+    window.open("https://github.com/Alecia1997/FlightsBookingSystem-Jquery.git");
+}
+function openBrief5() {
+  window.open('university.pdf');
+}
+
+function openCode5() {
+    window.open("https://github.com/Alecia1997/UniversityApplication-Java-JavaFX.git");
+}
+function openHonoursPaper() {
+  window.open('HonoursPaper.pdf');
+}
+
+function openBrief6() {
+  window.open('Doctor.pdf');
+}
+
+function openCode6() {
+    window.open("https://github.com/Alecia1997/DoctorOffice-PHP-mySQL.git");
+}
